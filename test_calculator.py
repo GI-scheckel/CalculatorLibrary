6d42f4544e0b0c314088c588d3aa70101a0b697b
GI-scheckel <sebastianscheckel@gmail.com>
2020-11-21 18:00:33 +0100

    Added some tests in test_calculator.py and removed some bug

---
+++ test_calculator.py
@@ -0,0 +1,14 @@
+"""
+Unit tests for the calculator library
+"""
+
+import calculator
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calculator.add(1, 3)
+
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4, 2)
