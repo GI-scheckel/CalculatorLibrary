abbb5992bd7fca5e8aaffc779354a8f143bb8298
GI-scheckel <sebastianscheckel@gmail.com>
2020-11-23 20:02:14 +0100

    Some changes for pylint

--- test_calculator.py
+++ test_calculator.py
@@ -10,17 +10,14 @@ class TestCalculator:
     Class to test the calculator.py file
     """
 
-    @classmethod
     def test_addition(self):
         """ Test for the add function """
         assert calculator.add(1, 3) == 4
-    
-    @classmethod
+
     def test_subtraction(self):
         """ Test for the subtract function """
         assert calculator.subtract(4, 2) == 2
 
-    @classmethod
     def test_multiplication(self):
         """ Test for the multiply function """
         assert calculator.multiply(10, 10) == 100
